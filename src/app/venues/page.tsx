339b48a3cd15c5c1deb1b79a1e62ef0afb558d23
lvncer <negimasa58@gmail.com>
2026-01-06 13:17:47 +0900

    feat: add useRouter to venues page for improved navigation handling

--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import LoadingScreen from "@/components/LoadingScreen";
 
